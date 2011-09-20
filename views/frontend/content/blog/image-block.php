f1199b77e5d69190ff839b67886b4067a7a4cb0b
stopkin <stopkin0@gmail.com>
2011-09-20 09:25:23 +0700

    fixes #73

--- views/frontend/content/blog/image-block.php
+++ views/frontend/content/blog/image-block.php
@@ -1,25 +1,29 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');?>
 <div class="frames" id="photos-right-box">
 <?php
-    foreach($images as $image):
+    $i = 0;
+    foreach($images as $image) {
+        if (file_exists(DOCROOT . $image->url)) {
+            $i++;
+            if ($i>6) break;
 ?>
 	<div class="frame">
 		<?php
 
-            /*$last_dot = strrpos($image->url, '.');
-            $thumb = substr($image->url, 0, $last_dot) . 'thumb' . substr($image->url, $last_dot);*/
-
             echo HTML::anchor($image->url,
                 HTML::image(
-				    Utils::get_thumb($image->url),
+                    Utils::get_thumb($image->url),
                     array('title' => $image->title,
                          'alt' => $image->title,
                          'width' => 100,
                          'height' => 73,
                          'id' => 'image-'.$image->id)),
-                array('rel' => 'fancybox-right'));
+                array('rel' => 'fancybox-right', 'title' =>  $image->title . '&nbsp;' .
+                        HTML::anchor(Route::get('blog_cars')->uri(array('action'=>'gallery', 'id'=>$image->car->id)),
+                            'Перейти в галерею')));
+
         ?>
 	</div>
-<?php endforeach;?>
+<?php } } ?>
 <div class="clear"></div>
 </div>
