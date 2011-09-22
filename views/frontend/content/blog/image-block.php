ad3ee73731232553017fe594f607deb898a31c62
stopkin <stopkin0@gmail.com>
2011-09-22 14:46:07 +0700

    modified fancybox out for right image block; need test

--- views/frontend/content/blog/image-block.php
+++ views/frontend/content/blog/image-block.php
@@ -12,7 +12,7 @@
 	<div class="frame">
 		<?php
 
-            echo HTML::anchor($path . '/' . $image->car->id . '/' . $image->url . '.' . $image->ext,
+            echo HTML::anchor('#'.$image->url /*$path . '/' . $image->car->id . '/' . $image->url . '.' . $image->ext*/,
                 HTML::image(
                     $path . '/' . $image->car->id . '/' . $image->url . '.thumb.' . $image->ext,
                     array('title' => $image->title,
@@ -20,11 +20,31 @@
                          'width' => 100,
                          'height' => 73,
                          'id' => 'image-'.$image->id)),
-                array('rel' => 'fancybox-right', 'title' =>  $image->title . '&nbsp;' .
-                        HTML::anchor(Route::get('blog_cars')->uri(array('action'=>'gallery', 'id'=>$image->car->id)),
-                            'Перейти в галерею')));
+                array('rel' => 'fancybox-right', 'title' =>  $image->title));
 
         ?>
+        <div style="display: none;">
+        <?php
+            $im = Image::factory($path . '/' . $image->car->id . '/' . $image->url . '.' . $image->ext);
+            $style = '';
+            if ($im->height<800)
+                $style .= 'height: '.$im->height . ';';
+            if ($im->width<1000)
+                $style .= 'width: '.$im->width . ';';
+            $img_atr = array();
+            if ($im->height>=800 or $im->width>=1000)
+                $img_atr = array('width'=>'100%', 'height'=>'100%');
+
+        ?>
+            <div id="<?php echo $image->url?>" style="<?php echo $style;?>">
+            <?php
+                echo HTML::image($path . '/' . $image->car->id . '/' . $image->url . '.' . $image->ext, $img_atr); ?>
+                <div style="width: 100%; position: absolute; top: 0px; text-align: center;">
+                <?php echo HTML::anchor(Route::get('blog_cars')->uri(array('action'=>'gallery', 'id'=>$image->car->id)),
+                            'Перейти в галерею'); ?>
+                </div>
+            </div>
+        </div>
 	</div>
 <?php } } ?>
 <div class="clear"></div>
